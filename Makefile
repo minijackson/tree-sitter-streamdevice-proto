98d49a348e43b82c827add40bdb02b082e50b7c1
Minijackson <minijackson@riseup.net>
2025-09-11 17:01:39 +0200

    chore: update to version 0.2.2

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ endif
 
 LANGUAGE_NAME := tree-sitter-streamdevice-proto
 HOMEPAGE_URL := https://github.com/minijackson/tree-sitter-streamdevice-proto
-VERSION := 0.2.1
+VERSION := 0.2.2
 
 # repository
 SRC_DIR := src
--- src/parser.c
+++ src/parser.c
@@ -17842,7 +17842,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_streamdevice_proto(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 1,
+      .patch_version = 2,
     },
   };
   return &language;
